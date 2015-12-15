dca2f89a4b3595b3ab17ea27e40558d13abbdf07
Steve Nolen <technolengy@gmail.com>
2015-12-15 10:26:20 -0800

    toying around still..

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures shib-oauth2-bridge'
 long_description 'Installs/Configures shib-oauth2-bridge'
-version '0.1.3'
+version '0.1.4'
 
 depends 'httpd', '~> 0.3.2'
