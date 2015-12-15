b9321cffff8becf9ebd724affe6a84c81d409050
Steve Nolen <technolengy@gmail.com>
2015-12-15 09:42:21 -0800

    stick to specific httpd

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures shib-oauth2-bridge'
 long_description 'Installs/Configures shib-oauth2-bridge'
-version '0.1.1'
+version '0.1.2'
 
 depends 'httpd'
