e6d03f1a9a5327f0e6b56b2c3db05881dc3124e8
Steve Nolen <technolengy@gmail.com>
2016-01-15 11:21:28 -0800

    don't create rails-like symlinks for this project

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures shib-oauth2-bridge'
 long_description 'Installs/Configures shib-oauth2-bridge'
-version '0.1.12'
+version '0.1.13'
 
 depends 'httpd', '~> 0.3.3'
